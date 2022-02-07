3f888c3acb7fdff597f4cb7281ff82557850a8f8
Wiebe, Alexander <=>
2022-02-07 16:13:01 -0600

    make the similar step to app1

--- projects/app2/src/app/app.component.ts
+++ projects/app2/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import * as vo from 'version-outputer-ts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'app2';
+  voText = vo.getVersion();
 }
