1ba207df795735998aab338f0f90f7f8c2bb91bd
Wiebe, Alexander <=>
2022-02-07 15:56:29 -0600

    update root pckage.json do: yarn install do: yarn workspace app1 start

--- projects/app1/src/app/app.component.ts
+++ projects/app1/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import * as vo from 'version-outputer-ts';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'app1';
+  voText = vo.getVersion();
 }
